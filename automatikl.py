d35f26906f55fb5a44286d2611dfdd9ced18f319
Erickbci <batista.erick07@hotmail.com>
2024-03-05 23:59:01 -0300

    Automatizacao de cadastro de produtos em app local

---
+++ automatikl.py
@@ -0,0 +1,28 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(991, 541,duration=0.4)
+pyautogui.write('erick')
+
+pyautogui.click(988, 563,duration=0.4)
+pyautogui.write('erick')
+
+pyautogui.click(869, 595,duration=0.4)
+
+with open('produtos.txt', 'r') as filename:
+        for line in filename:
+                produto = line.split(',')[0]
+                quantidade = line.split(',')[1]
+                preco = line.split(',')[2]
+
+                pyautogui.click(709, 530,duration=0.4)
+                pyautogui.write(produto)
+                
+                pyautogui.click(707, 555,duration=0.4)
+                pyautogui.write(quantidade)
+
+                pyautogui.click(713, 580,duration=0.4)
+                pyautogui.write(preco)
+                
+                pyautogui.click(593, 733,duration=0.4)
+                sleep(1)
